352461b584788c268a1238232194e533bfe65573
AAKASH <aakash291001@gmail.com>
2022-01-27 08:47:35 +0530

    2 Sum completed

---
+++ DS sheet/day 4/Two Sum.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> twosum(int A[],int sum,int n)
+{
+	vector<int> p;
+	for(int i=0;i<n-1;i++)
+	{
+		int curr = A[i];
+		for(int j=i+1;j<n;j++)
+		{
+			if(A[i]+A[j]==sum)
+			{
+				p.push_back(A[i]);
+				p.push_back(A[j]);
+				break;
+			}
+		}
+	}
+	return p;
+}
+vector<int> twosum2(int A[],int sum,int n)
+{
+	vector<int> p;
+	unordered_map<int,int> ump;
+	for(int i=0;i<n;i++)
+	{
+	    ump[A[i]]=1;
+	}
+	for(int i=0;i<n;i++)
+	{
+	    ump[A[i]]=0;
+	    if(ump[sum-A[i]]==1)
+	    {
+	        p.push_back(A[i]);
+	        p.push_back(sum-A[i]);
+	        break;
+	    }
+	}
+	return p;
+}
+int main()
+{
+	int A[] = {2,7,11,15};
+	vector<int> p;
+	p = twosum2(A,9,4);
+	cout<<p[0]<<" "<<p[1];
+	return 0;
+}
